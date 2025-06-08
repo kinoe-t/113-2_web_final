a89e6217893130e7b8d1ce94add748d070293a89
kinoe_T <kaeteyaruyo@gmail.com>
2025-06-08 23:25:07 +0800

    Modify 11610

--- 116/11610/server.py
+++ 116/11610/server.py
@@ -4,7 +4,7 @@ app = Flask(__name__, static_folder="")
 
 @app.route("/")
 def hello_world():
-   return send_file('index.html')
+   return send_file('fortune.html')
 
 if __name__ == "__main__":
    app.run(host="0.0.0.0", port=5000)
