84ea61606f7a847aed9b66e0ad474b043e12e7b6
kinoe_T <kaeteyaruyo@gmail.com>
2025-06-20 09:09:23 +0800

    Modify 11916

--- 119/11916/server.py
+++ 119/11916/server.py
@@ -46,7 +46,7 @@
 
   <script>
     const memes = [
-      { name: '不會讓你上', url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ' },
+      { name: '不會讓你上', url: 'https://www.youtube.com/embed/dQw4w9WgXcQ' },
       { name: '中立國特殊課程', url: 'https://www.youtube.com/watch?v=G2NngxegUBc' },
       { name: '這樣太危險', url: 'https://www.youtube.com/watch?v=1GG3YQ0P5_Y' },
       { name: '雲門舞集', url: 'https://www.youtube.com/watch?v=ZkNMZlkrzaU' },
@@ -58,7 +58,7 @@
       { name: '細胞壁', url: 'https://www.youtube.com/watch?v=Zd8bNW4DG5E' },
       { name: '進擊的巨人', url: 'https://www.youtube.com/watch?v=yBLdQ1a4-JI' },
       { name: '黃猿', url: 'https://www.youtube.com/watch?v=NfuiB52K7X8' },
-      { name: '阿罵', url: 'https://www.youtube.com/watch?v=ZhLDlYkb1K4' },
+      { name: '阿罵', url: 'https://www.youtube.com/embed/ZhLDlYkb1K4' },
       { name: '你鄰居很生氣', url: 'https://www.youtube.com/watch?v=dE7DCF_s7HQ' },
       { name: '芒果美語', url: 'https://www.youtube.com/watch?v=fzDZrPZ8s5c' },
       { name: '印度浦島太郎', url: 'https://www.youtube.com/watch?v=SfT4FMkh1-w' },
@@ -72,13 +72,13 @@
       { name: '雞腿換駕照', url: 'https://www.youtube.com/watch?v=7VFTcmGRM-k' },
       { name: '不可以', url: 'https://www.youtube.com/shorts/cPa0db6A9uc' },
       { name: '唧唧復唧唧', url: 'https://www.youtube.com/watch?v=WdA8AWKGE8w' },
-      { name: '喔喔喔喔喔喔喔喔', url: 'https://www.youtube.com/watch?v=Qkfyr-UpUp0' },
+      { name: '喔喔喔喔喔喔喔喔', url: 'https://www.youtube.com/embed/Qkfyr-UpUp0' },
       { name: '鳥', url: 'https://www.youtube.com/watch?v=nd0DtRU6M_c' },
       { name: '才沒有黃腔', url: 'https://www.youtube.com/watch?v=mBw3qzf4s18' },
-      { name: 'That’s beautiful ', url: 'https://www.youtube.com/watch?v=e0OggOfjiUE' },
-      { name: '小尖頭', url: 'https://www.youtube.com/watch?v=o7zqouVuFZY' },
+      { name: 'That’s beautiful ', url: 'https://www.youtube.com/watch?v=e0OggOfjiUE' },  
+      { name: '小尖頭', url: 'https://www.youtube.com/embed/o7zqouVuFZY' },
       { name: '丹丹漢堡', url: 'https://www.youtube.com/watch?v=CxoXItNERPM' },
-      { name: '嘟嘟嘟', url: 'https://www.youtube.com/watch?v=BbeeuzU5Qc8' },
+      { name: '嘟嘟嘟', url: 'https://www.youtube.com/embed/BbeeuzU5Qc8' },
     ];
 
     function draw() {
